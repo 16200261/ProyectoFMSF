8e977711013607938179ac0fc2e8e6d7e9de95d8
JhonathanCP <centtijhonathan@gmail.com>
2020-12-15 20:59:13 -0500

    project spring boot created

---
+++ backendfmsf/src/main/java/com/fmsf/BackendfmsfApplication.java
@@ -0,0 +1,13 @@
+package com.fmsf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendfmsfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendfmsfApplication.class, args);
+	}
+
+}
---
+++ backendfmsf/src/test/java/com/fmsf/BackendfmsfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmsf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendfmsfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
